39c309b20c84d254aef41fb8e5d51aa6245fd599
sanatan-dive <sanatansharma352@gmail.com>
2025-06-18 20:09:45 +0530

    removed playwright, improve code generation logic , its working but not replicating code

--- ai_agents/react/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Button({ variant = 'primary', children, onClick }) {
-  return (
-    <button className={`button ${variant}`} onClick={onClick}>
-      {children}
-    </button>
-  );
-}
